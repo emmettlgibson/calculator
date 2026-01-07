b0f4b3a306ed6baf8a2070df3f45f41d37017181
emmettlgibson <192617772+emmettlgibson@users.noreply.github.com>
2026-01-07 15:09:16 -0500

    Add buttons for operators and attach even listenrs

--- javascript.js
+++ javascript.js
@@ -40,15 +40,14 @@ const six = document.querySelector("#six");
 const seven = document.querySelector("#seven");
 const eight = document.querySelector("#eight");
 const nine = document.querySelector("#nine");
-const text = document.querySelector("#text");
 const clear = document.querySelector("#clear");
 
-text.style.overflow = "auto";
 
+
+/*
 clear.addEventListener("click", (e) => {
   text.textContent = "";
 })
-
 zero.addEventListener("click", (e) => {
   if (text.textContent === "0") {
     text.textContent = 0
@@ -56,63 +55,49 @@ zero.addEventListener("click", (e) => {
   text.textContent += 0;
 }
 });
-
 one.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 1
   } else {
-    text.textContent += 1;
+      text.textContent += 1;
   }
-});
-
+});  
 two.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
-      text.textContent = 2
+    text.textContent = 2
   } else {
     text.textContent += 2;
   }
-});
-
+});  
 three.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
-      text.textContent = 3;
+    text.textContent = 3;
   } else {
     text.textContent += 3;
   }
-});
-
+});  
 four.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 4;
   } else {
     text.textContent += 4;
   }
-});
-
+});  
 five.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 5;
   } else {
     text.textContent += 5;
   }
-});
-
+});  
 six.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 6;
   } else {
     text.textContent += 6;
   }
-});
-
+});  
 seven.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 7;
   } else {
@@ -121,26 +106,353 @@ seven.addEventListener("click", (e) => {
 });
 
 eight.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 8;
   } else {
     text.textContent += 8;
   }
-});
-
+});  
 nine.addEventListener("click", (e) => {
-
   if (text.textContent === "0") {
       text.textContent = 9;
   } else {
     text.textContent += 9;
   }
+})
+*/
+/*
+const division = document.querySelector("#division");
+const addition = document.querySelector("#addition")
+const mulitplication = document.querySelector("#multiply");
+const subtraction = document.querySelector("#subtract");
+const equal = document.querySelector("#equal");
+let operator = "";
+
+division.addEventListener("click", (e) => {
+operator = "/";
+console.log(operator);
+});
+addition.addEventListener("click", (e) => {
+  operator = "+";
+  console.log(operator);
+  });
+mulitplication.addEventListener("click", (e) => {
+  operator = "*";
+  console.log(operator);
+});  
+subtraction.addEventListener("click", (e) => {
+  operator = "-";
+  console.log(operator);
+});
+equal.addEventListener("click", (e) => {
+  operate(num1, operator, num2)
+});
+*/
+
+
+
+
+
+
+const text = document.querySelector("#text");
+//Doesn't add to num1
+//Next use = to add num1 and num2
+//Set numbers to a function
+function zeroFunc() {
+  zero.addEventListener("click", (e) => {
+
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = 0
+      num1 += 0;
+  } else {
+    text.textContent += 0;
+    num1 += 0;
+  }
+} else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+  if (text.textContent === "0") {
+    text.textContent = 0
+    num2 += 1;
+} else {
+  text.textContent += 0;
+  num2 += 1;
+}
+}
+
+//Set operator = '' when = is clicked
+})
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let num1 = '';
+let num2 = '';
+
+
+
+
+
+clear.addEventListener("click", (e) => {
+  text.textContent = "";
+  num1 = '';
+  num2 = '';
+  operator = '';
 });
 
 
+
+zero.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '0'
+      num1 = '0';
+  } else {
+    text.textContent += '0';
+    num1 += '0';
+  }
+  } 
+  
+  if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '0'
+      num2 = '0';
+  } else {
+    text.textContent += '0';
+    num2 += '0';
+  }
+  } 
+  
+  if (operator != '' && num1 !== '' && num2 !== '') {
+    text.textContent = num2;
+}
+
+});
+
+
+
+
+
+one.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '1';
+      num1 = '1';
+  } else {
+    text.textContent += '1';
+    num1 += '1';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '1'
+      num2 = '1';
+  } else {
+    text.textContent += '1';
+    num2 += '1';
+  }
+  }
+});
+two.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '2';
+      num1 = '2';
+  } else {
+    text.textContent += '2';
+    num1 += '2';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '2'
+      num2 = '2';
+  } else {
+    text.textContent += '2';
+    num2 += '2';
+  }
+  }
+});
+three.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '3';
+      num1 = '3';
+  } else {
+    text.textContent += '3';
+    num1 += '3';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '3'
+      num2 = '3';
+  } else {
+    text.textContent += '3';
+    num2 += '3';
+  }
+  }
+});
+four.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '4';
+      num1 = '4';
+  } else {
+    text.textContent += '4';
+    num1 += '4';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '4'
+      num2 = '4';
+  } else {
+    text.textContent += '4';
+    num2 += '4';
+  }
+  } 
+});
+five.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '5';
+      num1 = '5';
+  } else {
+    text.textContent += 5;
+    num1 += '5';
+  }
+  }  else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '5'
+      num2 = '5';
+  } else {
+    text.textContent += '5';
+    num2 += '5';
+  }
+  }
+
+});
+six.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '6';
+      num1 = '6';
+  } else {
+    text.textContent += '6';
+    num1 += '6';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '6'
+      num2 = '6';
+  } else {
+    text.textContent += '6';
+    num2 += '6';
+  }
+  } 
+});
+seven.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '7';
+      num1 = '7';
+  } else {
+    text.textContent += '7';
+    num1 += '7';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '7'
+      num2 = '7';
+  } else {
+    text.textContent += '7';
+    num2 += '7';
+  }
+  }
+});
+eight.addEventListener("click", (e) => {
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '8';
+      num1 = '8';
+  } else {
+    text.textContent += '8';
+    num1 += '8';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '8'
+      num2 = '8';
+  } else {
+    text.textContent += '8';
+    num2 += '8';
+  }
+  }
+});
+nine.addEventListener("click", (e) => {
+
+  if (operator === '') {
+    if (text.textContent === "0") {
+      text.textContent = '9';
+      num1 = '9';
+  } else {
+    text.textContent += '9';
+    num1 += '9';
+  }
+  } else if (operator === '*' || operator === '+' || operator === '/' || operator === '-') {
+    if (text.textContent === "0") {
+      text.textContent = '9'
+      num2 = '9';
+  } else {
+    text.textContent += '9';
+    num2 += '9';
+  }
+  }
+});
+
 const division = document.querySelector("#division");
+const addition = document.querySelector("#addition")
+const mulitplication = document.querySelector("#multiply");
+const subtraction = document.querySelector("#subtract");
+const equal = document.querySelector("#equal");
+let operator = "";
 
 division.addEventListener("click", (e) => {
-operate();
-})
+operator = "/";
+num1 = +num1;
+console.log(operator);
+});
+addition.addEventListener("click", (e) => {
+  operator = "+";
+  num1 = +num1;
+  console.log(operator);
+  });
+mulitplication.addEventListener("click", (e) => {
+  operator = "*";
+  num1 = +num1;
+  console.log(operator);
+});  
+subtraction.addEventListener("click", (e) => {
+  operator = "-";
+  num1 = +num1;
+  console.log(operator);
+});
+equal.addEventListener("click", (e) => {
+  operate(num1, operator, num2)
+});
+
+
+
+
+
